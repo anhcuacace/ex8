130380b9fc4989402fe5776af8be9ceb746d95c0
Anh <trantuananh291299@gmail.com>
2022-06-12 14:42:24 +0700

    update

--- app/src/main/java/com/tunanh/myapplication/main/MainActivity.kt
+++ app/src/main/java/com/tunanh/myapplication/main/MainActivity.kt
@@ -3,31 +3,38 @@ package com.tunanh.myapplication.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toolbar
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.tunanh.myapplication.R
 import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
-    var rcv_folder: RecyclerView? = null
+
     var folderList: MutableList<SinhVien> = ArrayList()
     var toolbar: Toolbar? = null
 
     var adapter: SInhVienAdapter? = null
     var databaseFolder: SinhVienHelper? = null
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var rcv=findViewById<RecyclerView>(R.id.rcl_view)
+        rcv.layoutManager=LinearLayoutManager(this)
+        folderList.add(SinhVien(1,"anh","quynh coi","0225675563"))
+        adapter= SInhVienAdapter(folderList,this)
+        rcv.adapter=adapter
 
     }
-    companion object {
-        const val REQUEST_CODE_ADD_ITEM = 123
-        const val REQUEST_CODE_EDIT_ITEM = 1234
-        const val ACTION_ADD = "add"
-        const val ACTION_EDIT = "edit"
-        const val ITEM_SV = "item_sinhvien"
-        const val LIST_SV = "list_sv"
-        const val NAME_FOLDER = "name"
-        const val DESCRIPTION_FOLDER = "description"
-    }
+//    companion object {
+//        const val REQUEST_CODE_ADD_ITEM = 123
+//        const val REQUEST_CODE_EDIT_ITEM = 1234
+//        const val ACTION_ADD = "add"
+//        const val ACTION_EDIT = "edit"
+//        const val ITEM_SV = "item_sinhvien"
+//        const val LIST_SV = "list_sv"
+//        const val NAME_FOLDER = "name"
+//        const val DESCRIPTION_FOLDER = "description"
+//    }
 }
--- app/src/main/java/com/tunanh/myapplication/main/SInhVienAdapter.kt
+++ app/src/main/java/com/tunanh/myapplication/main/SInhVienAdapter.kt
@@ -1,9 +1,38 @@
 package com.tunanh.myapplication.main
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.tunanh.myapplication.R
 
 
-class SInhVienAdapter(sinhVien: MutableList<SinhVien>,context: MainActivity) :
+class SInhVienAdapter(private val sinhVien: MutableList<SinhVien>,context: MainActivity) :
     RecyclerView.Adapter<SInhVienAdapter.MyViewHolder>() {
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var name: TextView= itemView.findViewById(R.id.names)
+        var address: TextView= itemView.findViewById(R.id.addresss)
+        var phone: TextView= itemView.findViewById(R.id.phones)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val view= LayoutInflater.from(parent.context).inflate(R.layout.sinhvien,parent,false)
+        return MyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val itemsinhVien = sinhVien[position]
+        holder.name.text=itemsinhVien.name
+        holder.address.text=itemsinhVien.address
+        holder.phone.text=itemsinhVien.phone
+
+    }
+
+    override fun getItemCount(): Int {
+
+             return sinhVien.size
+
+    }
 
 }
